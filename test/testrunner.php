196115171f7b0ea3e1955031e08e3d0e09a3181f
moisesb08 <13moii@gmail.com>
2018-04-03 12:23:21 -0700

    Updated testrunner.php to expect testing of Post, for story-156458609

--- test/testrunner.php
+++ test/testrunner.php
@@ -1,8 +1,9 @@
 <?php
 
-require('User_test.php');
-require('Forum_test.php');
-require('Thread_test.php');
+include('User_test.php');
+include('Forum_test.php');
+include('Thread_test.php');
+include('Post_test.php');
 
 echo "\nRunning User tests\n";
 $ut = new User_test();
@@ -12,4 +13,7 @@ $ft = new Forum_test();
 
 echo "\n\nRunning Thread tests";
 $tt = new Thread_test();
+
+echo "\n\nRunning Post tests";
+$pt = new Post_test();
 ?>
